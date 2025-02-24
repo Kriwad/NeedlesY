909a680d241b0067044c6d4d18f9547b2e5097b3
Kriwad <khatiwadakrish84@gmail.com>
2025-02-24 11:48:40 +0400

    some change

--- Backend/backend/backend/settings.py
+++ Backend/backend/backend/settings.py
@@ -152,27 +152,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_METHODS = [
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-]
 
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
+
 
 
 AUTH_USER_MODEL = 'api.CustomUser'
